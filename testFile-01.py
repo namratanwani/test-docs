8c9214048b4d5407b14b1b247c648af7fcdcd3b8
namratanwani <tanwaninamrata11@gmail.com>
2024-05-10 13:59:21 +0100

    docs

---
+++ testFile-01.py
@@ -0,0 +1,24 @@
+def load_model(itos_filename, classifier_filename, num_classes):
+
+    # load the int to string mapping file
+    itos = pickle.load(Path(itos_filename).open('rb'))
+    # turn it into a string to int mapping (which is what we need)
+    stoi = collections.defaultdict(lambda:0, {str(v):int(k) for k,v in enumerate(itos)})
+
+    # these parameters aren't used, but this is the easiest way to get a model
+    bptt,em_sz,nh,nl = 70,400,1150,3
+    dps = np.array([0.4,0.5,0.05,0.3,0.4])*0.5
+    vs = len(itos)
+
+    model = get_rnn_classifer(bptt, 20*70, num_classes, vs, emb_sz=em_sz, n_hid=nh, n_layers=nl, pad_token=1,
+            layers=[em_sz*3, 50, num_classes], drops=[dps[4], 0.1],
+            dropouti=dps[0], wdrop=dps[1], dropoute=dps[2], dropouth=dps[3])
+
+    # load the trained classifier
+    model.load_state_dict(torch.load(classifier_filename, map_location=lambda storage, loc: storage))
+
+    # put the classifier into evaluation mode
+    model.reset()
+    model.eval()
+
+    return stoi, model
